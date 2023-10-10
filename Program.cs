5f0eb9eedb5a77705f5ee52a9b946c4caded8600
Jaydavisetsu <113927036+Jaydavisetsu@users.noreply.github.com>
2023-10-10 19:45:58 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,182 @@
+﻿using System.Text.Json;
+using Newtonsoft.Json;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+//using ConsumingAPIs;
+
+
+namespace Lab4_API_Jayden
+{
+    public class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            await ZeldaCall();
+
+            using var httpClient = new HttpClient();
+
+            string apiUrl = $"https://zelda.fanapis.com/api/games?name";
+
+
+            // Display a menu to look up games
+            while (true)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Search for a game by name");
+                Console.WriteLine("2. Exit");
+                Console.Write("Enter your choice: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+
+                        Console.Write("Enter the name of the game: ");
+                        string gameName = Console.ReadLine();
+
+                        try
+                        {
+                            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                            if (response.IsSuccessStatusCode)
+                            {
+                                string content = await response.Content.ReadAsStringAsync();
+                                Console.WriteLine(content);
+
+                                Console.WriteLine("\n\n\n");
+
+                                dynamic item = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result);
+
+                                // Console.WriteLine($"Game Searched: {item.name}");
+                            }
+
+                            /*   string jsonSerializedData = "{\"name\": \"Value1\", \"developer\": \"Value2\"}";
+                               ZeldaData zeldaData = ZeldaData.DeserializeFromJson(jsonSerializedData);
+
+                               if (zeldaData != null)
+                               {
+                                   // Now, 'zeldaData' contains the deserialized object
+                                   Console.WriteLine("Title: " + zeldaData.Name);
+                                   Console.WriteLine("Dev(s): " + zeldaData.Developer);
+                               }*/
+                            else
+                            {
+                                Console.WriteLine($"Error : {response.StatusCode}");
+                            }
+                        }
+                        catch (HttpRequestException ex)
+                        {
+                            Console.WriteLine($"HTTP Request error: {ex.Message}");
+                        }
+                        break;
+
+                    case "2":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(content);
+
+                        Console.WriteLine("\n\n\n");
+
+                        dynamic item = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result);
+
+                        //Console.WriteLine($"https://zelda.fanapis.com/api/games?name");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error : {response.StatusCode}");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"HTTP Request error: {ex.Message}");
+                }
+
+
+            }
+        }
+
+        /*     static void SearchGameByName(List<ZeldaData> games, string name)
+             {
+                 foreach (var game in games)
+                 {
+                     if (game.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                     {
+                         Console.WriteLine("Game Name: " + game.Name);
+                         Console.WriteLine("Description: " + game.Description);
+                         Console.WriteLine("Developer: " + game.Developer);
+                         Console.WriteLine("Publisher: " + game.Publisher);
+                         Console.WriteLine("Released Date: " + game.ReleasedDate.ToShortDateString());
+                         Console.WriteLine();
+                         return;
+                     }
+                 }
+
+                 Console.WriteLine("Game not found.");
+             }*/
+
+        public static async Task ZeldaCall()
+        {
+            /*        https://zelda.fanapis.com/api/games?name
+
+                        var client = new HttpClient();
+
+                        HttpResponseMessage response = await client.GetAsync("https://zelda.fanapis.com/api/games?name");
+
+                        string json = await response.Content.ReadAsStringAsync();
+
+                        var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+
+                        ZeldaData? p = JsonSerializer.Deserialize<ZeldaData?>(options);
+                        ZeldaData? p = JsonSerializer.Deserialize<ZeldaData?>(json);
+                        Console.WriteLine(json + "\n"); */
+
+            var client = new HttpClient();
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://zelda.fanapis.com/api/games?name");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+                    // Deserialize the JSON string into a ZeldaData object
+                    ZeldaData? zeldaData = JsonSerializer.Deserialize<ZeldaData?>(json, options);
+
+                    if (zeldaData != null)
+                    {
+                        Console.WriteLine($"Name: {zeldaData.Name}");
+                        // Access other properties as needed
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Failed to retrieve data from the API.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+    
+}
+
+           
---
+++ ZeldaData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Lab4_API_Jayden
+{
+    public class ZeldaData
+    {
+        public string Name { get; set; }
+       // public string Description { get; set; }
+        public string Developer { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleasedDate { get; set; }
+        public List<ZeldaData> dataList { get; set; } = new List<ZeldaData>();
+
+
+
+        public ZeldaData(string name, string description, string developer, string publisher, DateTime releasedDate)
+        {
+            this.Name = name;
+        //    this.Description = description;
+            this.Developer = developer;
+            this.Publisher = publisher;
+            this.ReleasedDate = releasedDate;
+
+        }
+
+        public ZeldaData() { }
+
+        public static ZeldaData DeserializeFromJson(string jsonString)
+        {
+            try
+            {
+                ZeldaData zeldaData = JsonConvert.DeserializeObject<ZeldaData>(jsonString);
+                return zeldaData;
+            }
+            catch (JsonException ex)
+            {
+                // Handle JSON deserialization errors here
+                Console.WriteLine("Error deserializing JSON: " + ex.Message);
+                return null;
+            }
+        }
+
+
+    }
+
+}
